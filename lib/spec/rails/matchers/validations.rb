cb5c3aa4e9f346ca9920ffd5e0d0835144de3dcd
Christoph Bünte <info@christophbuente.de>
2009-04-16 19:19:24 +0200

    added matcher for numericality

--- lib/spec/rails/matchers/validations.rb
+++ lib/spec/rails/matchers/validations.rb
@@ -37,6 +37,65 @@ module Spec
           invalid
         end
       end
+      
+      def validate_numericality_of(attribute, options = {})
+        
+        blank = max = min = odd = even = nil
+        
+        if options.blank?
+          blank = true                  
+        elsif options.has_key? :greater_than          
+          min = (options[:greater_than] + 1)
+        elsif options.has_key? :greater_than_or_equal_to
+          min = options[:greater_than_or_equal_to]
+        elsif options.has_key? :equal_to
+          min = options[:equal_to] 
+          max = options[:equal_to]
+        elsif options.has_key? :less_than
+          max = (options[:less_than] - 1)
+        elsif options.has_key? :less_than_or_equal_to
+          max = options[:less_than_or_equal_to]
+        end
+
+        if options.has_key? :odd
+          odd = true
+        elsif options.has_key? :even
+          even = true
+        end
+        
+        return simple_matcher("model to validate #{attribute} to be a number between #{min || 0} and #{max || 'Infinity'} inclusive") do |model|
+          invalid = true
+          if !min.nil?
+            model.send("#{attribute}=", (min - 1))
+            invalid &&= !model.valid? && model.errors.invalid?(attribute)
+          end
+          
+          if !max.nil?
+            model.send("#{attribute}=", (max + 1))
+            invalid &&= !model.valid? && model.errors.invalid?(attribute)
+          end
+          
+          if !odd.nil?
+            value = [(min || 0), (max || 2)].min
+            value += 1 if value.odd?
+            model.send("#{attribute}=", (value))
+            invalid &&= !model.valid? && model.errors.invalid?(attribute)
+          end
+
+          if !even.nil?
+            value = [(min || 0), (max || 2)].min
+            value += 1 if value.even?
+            model.send("#{attribute}=", (value))
+            invalid &&= !model.valid? && model.errors.invalid?(attribute)
+          end
+          
+          if !blank.nil?
+            model.send("#{attribute}=", ('character'))
+            invalid &&= !model.valid? && model.errors.invalid?(attribute)
+          end
+          invalid
+        end
+      end
 
       def validate_uniqueness_of(attribute)
         return simple_matcher("model to validate the uniqueness of #{attribute}") do |model|
